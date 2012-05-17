6c270311f82b56eae8538c2baf9e5957dc7aad3f
Luke Shepard <luke@lukeshepard.com>
2012-05-16 23:03:46 -0700

    Including changed files too.

--- html/js/core.js
+++ html/js/core.js
@@ -1,10 +1,26 @@
 function init_table() {
   
   var options = fetch_data_options();
-  options['bPaginate'] = false;
+  //  options['bPaginate'] = false;
   options['sErrMode'] = 'throw';
 
-  $('#models').dataTable(options);
+  // Remove the option to choose a pagination length
+  options['iDisplayLength'] = 20;
+  options['sDom'] = 'frtip'; // this is the default minus "l"ength
+  
+  var oTable = $('#models').dataTable(options);
+  $('#program_models').
+    multiselect({header: false,
+                 selectedList: 3,
+          noneSelectedText: "",
+          checkAllText: "All",
+          uncheckAllText: "None",
+          height: "auto"});
+
+  $('#program_models').change(function() { 
+    console.log('onchange'); 
+    oTable.fnDraw(); 
+    });
 }
 
 // Convert from the incoming JSON hash
@@ -29,7 +45,8 @@ function fetch_data_options() {
                model['gradesserved'] ? model['gradesserved'] : '',
                model['blendedgrades'] ? model['blendedgrades'] : '',
                model['blendedenrollment'] ? model['blendedenrollment'] : '',
-               model['blendedsubjects'] ? model['blendedsubjects'] : ''
+               model['blendedsubjects'] ? model['blendedsubjects'] : '',
+               model['programmodels'] ? model['programmodels'] : ''
                ]);
   }
 
@@ -47,7 +64,38 @@ function fetch_data_options() {
                 {'sTitle': 'Grades Served'},
                 {'sTitle': 'Blended Grades'},
                 {'sTitle': 'Blended Enrollment'},
-                {'sTitle': 'Blended Subjects'}
+                {'sTitle': 'Blended Subjects'},
+                {'sTitle': 'Program Models'}
                 ],
     aaData: data};
 }
+
+// Modified filtering plugins pulled from http://datatables.net/plug-ins/filtering
+
+$.fn.dataTableExt.afnFiltering.push
+  (
+   function (oSettings, aData, iDataIndex) {
+     var oSelect = document.getElementById('program_models');
+     var iColumn = 11;
+
+     console.log("custom filter on ", aData[iColumn].split(','));
+
+     // get an object with keys for each comma-delimited element here
+     // n^2 algo but each one should be really small so no big deal
+     var potential_matches = aData[iColumn].split(',');
+
+     var nothingSelected = true;
+     for (var i = 0; i < oSelect.options.length; ++i) {
+       if (oSelect.options[i].selected) {
+         nothingSelected = false;
+         // now look through all the objects in split
+         for (var j = 0; j < potential_matches.length; ++j) {
+           if (potential_matches[j].indexOf(oSelect.options[i].value) != -1) {
+             return true;
+           }
+         }
+       }
+     }
+
+     return nothingSelected; // default to all if nothing is selected
+   });
